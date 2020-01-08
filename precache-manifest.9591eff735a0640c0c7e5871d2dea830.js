bbe3777227d06621caf8d742c0c2944b90c362da
Ron Dayan <Ron13dayan@gmail.com>
2020-01-08 14:10:15 +0200

    Adding the robots project to git hub

---
+++ precache-manifest.9591eff735a0640c0c7e5871d2dea830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2741a9ce181678bee7f79028b230742",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "129b268c74c945644fb9",
+    "url": "/Robots/static/css/main.7f684576.chunk.css"
+  },
+  {
+    "revision": "b25eac3eda2faed1c802",
+    "url": "/Robots/static/js/2.1cd57664.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robots/static/js/2.1cd57664.chunk.js.LICENSE"
+  },
+  {
+    "revision": "129b268c74c945644fb9",
+    "url": "/Robots/static/js/main.a3fa5d99.chunk.js"
+  },
+  {
+    "revision": "501f285ee42704f030e6",
+    "url": "/Robots/static/js/runtime-main.8e28a55b.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/Robots/static/media/SEGA.82e09f78.TTF"
+  }
+]);
