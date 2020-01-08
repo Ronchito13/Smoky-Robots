809b3e76eeba58d6a8aa1d866c382a8eefc23bf6
Ron Dayan <Ron13dayan@gmail.com>
2020-01-08 14:33:42 +0200

    Updates

---
+++ precache-manifest.7f238ccf70fbac821d2205f3bacdbd75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b5314fff5b242e99279c27ed6dbe88",
+    "url": "/Ronchito13/Smoky-Robots/index.html"
+  },
+  {
+    "revision": "8021defc20e67aac18d4",
+    "url": "/Ronchito13/Smoky-Robots/static/css/main.84a8a4e4.chunk.css"
+  },
+  {
+    "revision": "b25eac3eda2faed1c802",
+    "url": "/Ronchito13/Smoky-Robots/static/js/2.1cd57664.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Ronchito13/Smoky-Robots/static/js/2.1cd57664.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8021defc20e67aac18d4",
+    "url": "/Ronchito13/Smoky-Robots/static/js/main.629117fc.chunk.js"
+  },
+  {
+    "revision": "d6102fa9dbb9e8d784cc",
+    "url": "/Ronchito13/Smoky-Robots/static/js/runtime-main.9d81c65c.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/Ronchito13/Smoky-Robots/static/media/SEGA.82e09f78.TTF"
+  }
+]);
